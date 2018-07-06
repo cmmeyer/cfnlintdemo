27d631317ad06f2c0d55ce9c52afaf529fe1849c
Chuck Meyer <cmmeyer@amazon.com>
2018-07-05 22:17:01 -0400

    Simplified linter step to single command line

--- tests/multi-lint.sh
+++ tests/multi-lint.sh
@@ -1,24 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Let's lint all these file"
-
-fail_files=()
-
-cd templates
-for filename in *; do
-  echo $filename
-  if cfn-lint --template $filename; then
-    echo "Nothing wrong with $filename"
-  else
-    echo "Something is wrong with $filename"
-    fail_files+=($filename)
-  fi
-  #cfn-lint --template $filename &> linter-output/${filename}-now --ignore-checks W
-done
-cd ..
-
-if [ ${#fail_files[@]} == 0 ]; then
-  exit 0
-else
-  exit 1
-fi
+for filename in templates/*; do echo $filename; cfn-lint --template $filename; done
